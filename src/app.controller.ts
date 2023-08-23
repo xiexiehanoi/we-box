9c15cf97f28fee9308368b0cfdfc6c1fecffe170
김상우 <gimsang-u@Leeui-MacBook-Pro.local>
2023-08-24 00:22:15 +0900

    초기 셋팅

---
+++ src/app.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AppService } from './app.service';
+import { AuthGuard } from '@nestjs/passport';
+
+@Controller('google')
+export class AppController {
+  getHello: any;
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @UseGuards(AuthGuard('google'))
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async googleAuth(@Req() req) {}
+
+  @Get('redirect')
+  @UseGuards(AuthGuard('google'))
+  googleAuthRedirect(@Req() req) {
+    return this.appService.googleLogin(req);
+  }
+}
